033aa64bf34c35f44c1198ebf352d1b9b93c5692
Nassi Harel <nassi9@gmail.com>
2018-02-15 11:50:32 +0200

    add consts and events

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Producer = require('./lib/producer/producer');
 const Consumer = require('./lib/consumer/consumer');
+const Events = require('./lib/consts/Events');
 
 module.exports = {
     Producer,
-    Consumer
+    Consumer,
+    Events
 };
